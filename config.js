cfa7d91f6efaec643023b28ad556f20df5666997
Gimantha Dilshan <90886839+gimantha-d@users.noreply.github.com>
2024-07-25 18:16:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -84,7 +84,7 @@ module.exports = {
   KOYEB_API : process.env.KOYEB_API  || "false",
 
   REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "",
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "sk-svcacct-d3Ypl36iQFVVYOBzn6mdT3BlbkFJcnmt7toJMaHC2cVzXKHu",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "sk-None-maFeoiMxVcJhOHaqhMmUT3BlbkFJFA6NQJR1UPMUZNsvvLYL",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "",
   antilink_values:process.env.ANTILINK_VALUES|| "all",
